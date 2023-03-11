69da50cd94cc6988e3ef139b0ec4e5f5ad7ce3f0
mishuk09 <mishukinfo09@gmail.com>
2023-03-11 13:09:17 -0500

    project

--- login_register/user_profile_pic/index.php
+++ login_register/user_profile_pic/index.php
@@ -34,6 +34,41 @@ if (isset($_POST['upload'])) {
     <title>Image Upload</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css" />
+
+
+
+	<style>
+		*{
+	margin: 0;
+	padding: 0;
+	 
+}
+
+#content{
+	width: 50%;
+	justify-content: center;
+	align-items: center;
+	margin: 20px auto;
+	border: 1px solid #cbcbcb;
+}
+form{
+	width: 50%;
+	margin: 20px auto;
+}
+
+#display-image{
+	width: 100%;
+	justify-content: center;
+	padding: 5px;
+	margin: 15px;
+}
+img{
+	margin: 5px;
+	width: 350px;
+	height: 250px;
+}
+
+	</style>
 </head>
 
 <body>
